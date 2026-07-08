08c63e4879cf853f7d548aa697ff81ba11a42e18
syjy813 <73263769+syjy813@users.noreply.github.com>
2026-07-08 13:13:28 +0900

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://syjy813.github.io',
   base: '/getpasslab',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
